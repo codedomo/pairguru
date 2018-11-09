260749287687aeeeb6dec0e13bd234faca610d57
Dominik Spisak <spisak.dominik@gmail.com>
2018-11-09 02:14:06 +0100

    Add methods to movie model and alter config to allow lib

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,4 +22,27 @@ class Movie < ApplicationRecord
   validates_associated :comments
 
   accepts_nested_attributes_for :comments, allow_destroy: true
+
+  attr_accessor :details
+
+  AVATAR_PATH = 'https://pairguru-api.herokuapp.com/'.freeze
+
+  def avatar
+    "#{AVATAR_PATH}#{details.poster}"
+  end
+
+  def rating
+    details.rating
+  end
+
+  def description
+    details.plot
+  end
+
+  private
+
+  def details
+    @details ||= MyApi::Movie.find(title).first
+  end
+
 end
